07b237d152cc03487e7c973fff4810a5e7bbfd9c
ljubomir.cakarevic <ljubomir.cakarevic@gmail.com>
2020-12-12 00:18:30 +0100

    Request Model, Response Model, Dto and Entity

---
+++ src/main/java/com/appdeveloperblog/ws/io/entity/UserEntity.java
@@ -0,0 +1,107 @@
+package com.appdeveloperblog.ws.io.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="users")
+public class UserEntity implements Serializable {
+	//this class will be used to store user details in database
+
+	private static final long serialVersionUID = 6306439645377478599L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false)
+	private String userId;
+	
+	@Column(nullable=false, length=50)
+	private String firstName;
+	
+	@Column(nullable=false, length=50)
+	private String lastName;
+	
+	@Column(nullable=false, length=120)
+	private String email;
+	
+	@Column(nullable=false)
+	private String encryptedPassword;
+	
+	private String emailVerificationToken;
+	
+	@Column(nullable=false, columnDefinition = "boolean default value")
+	private String emailVerificationStatus;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+
+	public String getEmailVerificationToken() {
+		return emailVerificationToken;
+	}
+
+	public void setEmailVerificationToken(String emailVerificationToken) {
+		this.emailVerificationToken = emailVerificationToken;
+	}
+
+	public String getEmailVerificationStatus() {
+		return emailVerificationStatus;
+	}
+
+	public void setEmailVerificationStatus(String emailVerificationStatus) {
+		this.emailVerificationStatus = emailVerificationStatus;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/appdeveloperblog/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.appdeveloperblog.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.appdeveloperblog.ws.service.UserService;
+import com.appdeveloperblog.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
